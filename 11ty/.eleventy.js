83d65de58132ce47c99f6f7e6261f4dd4aff31a8
EetaZeeba <46096410+eetazeeba@users.noreply.github.com>
2026-02-25 23:45:25 -0700

    Making a mess

---
+++ 11ty/.eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/CSS");
+  eleventyConfig.addPassthroughCopy("src/Images");
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    },
+    templateFormats: ["html", "njk", "md"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
